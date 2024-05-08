d01decde0b2241631d2f2db7f67714fa7cf9b7c1
devanshkr <devanshkumar2000dk@gmail.com>
2024-05-08 07:02:55 +0530

    pushing code

---
+++ src/main/java/com/springApis/blog/BloggingAppApIsApplication.java
@@ -0,0 +1,13 @@
+package com.springApis.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingAppApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingAppApIsApplication.class, args);
+	}
+
+}
